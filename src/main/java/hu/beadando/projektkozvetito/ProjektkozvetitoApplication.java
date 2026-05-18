c7fc2d08f5eb71186bb4450f39d107da4cd27ef7
PappJani <pappjanosdavid@gmail.com>
2026-05-18 19:29:50 +0200

    init: Spring Boot alap letrehozasa

---
+++ src/main/java/hu/beadando/projektkozvetito/ProjektkozvetitoApplication.java
@@ -0,0 +1,13 @@
+package hu.beadando.projektkozvetito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektkozvetitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektkozvetitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/beadando/projektkozvetito/ProjektkozvetitoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.beadando.projektkozvetito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektkozvetitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
